4c47c74eaa5be7ef81d32d4cca6e333f86d8f8ff
kevinz-uw <kevinz@cs.washington.edu>
2012-09-05 20:43:54 -0700

    Turn off the rufus scheduler in heroku (since it is not dependable there).

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,20 +4,22 @@ require 'scirate/recommenders/recommenders'
 require 'scirate/logging'
 
 
-scheduler = Rufus::Scheduler.start_new
+if ENV['RUFUS_SCHEDULER'] != 'no'
+  scheduler = Rufus::Scheduler.start_new
 
-scheduler.cron '45 17 * * *' do
-  Arxiv::crawl
-end
+  scheduler.cron '45 17 * * *' do
+    Arxiv::crawl
+  end
 
-scheduler.cron '45 16 * * *' do
-  Recommenders::update_user_models
-end
+  scheduler.cron '45 16 * * *' do
+    Recommenders::update_user_models
+  end
 
-scheduler.cron '30 16 * * *' do
-  Recommenders::update_global_model
-end
+  scheduler.cron '30 16 * * *' do
+    Recommenders::update_global_model
+  end
 
-scheduler.cron '00 00 * * *' do
-  Logging::clean
+  scheduler.cron '00 00 * * *' do
+    Logging::clean
+  end
 end
